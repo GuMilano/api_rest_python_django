9ee9a43ba05591ba4d0c0c0887df4e5e027f05e0
GuMilano <gustavo.fenex@hotmail.com>
2026-02-13 09:55:25 -0300

    Initial commit

---
+++ cursos/views2.py
@@ -0,0 +1,39 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+class CursoAPIView(APIView):
+    """
+    API de Cursos da Geek
+    """
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        #return Response(serializer.data, status=status.HTTP_201_CREATED)
+        #return Response({"msg" : "Criou com sucesso"}, status=status.HTTP_201_CREATED)
+        return Response({"id": serializer.data["id"], "curso": serializer.data["titulo"]}, status=status.HTTP_201_CREATED)
+
+
+class AvaliacaoAPIView(APIView):
+    """
+    API de Avaliacao da Geek
+    """
+    def get(self, request):
+        avaliacaos = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacaos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
